3dc47bf6f3dc278eb93dcc083891c76e864ca045
KJC <kevin94chen@gmail.com>
2020-05-09 15:46:08 +0800

    autofill for visera

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ import configparser
 from time import gmtime, strftime
 
 Timestamp = strftime("%d%b%Y%H%M%S")
-url = 'https://zh.surveymonkey.com/r/EmployeeHealthCheck?fbclid=IwAR1gE86MoL4RoPFxpyigUXuV2HIHfoY9epKwmAqrrIMYl8O_s6Z3O09qqmo'
+url = 'http://219.87.157.3:8080/bt/Bt'
 
 
 def _read_config():
     config = configparser.ConfigParser()
     config.read('info.ini')
 
-    i = config['default']['tsmcID']
+    i = config['default']['ID']
     t = config['default']['tempature']
     if t == "":
         t = "36"
@@ -20,28 +20,43 @@ def _read_config():
 
 
 def main(id, body_temp):
-    radio_btn_agree = driver.find_element_by_id("430334642_2860832437")
-    radio_btn_agree.send_keys(Keys.SPACE)
-    tsmc_id = driver.find_element_by_id("430334639")
-    tsmc_id.send_keys(id)
-    temp_meas_mthd = driver.find_element_by_id("430334644_2860832441")
-    temp_meas_mthd.send_keys(Keys.SPACE)
-    temp = driver.find_element_by_id("430334640")
+
+    v_id = driver.find_element_by_id("empId")
+    v_id.send_keys(id)
+
+    temp_meas_mthd = driver.find_element_by_xpath("//select[@name='tmpType']/option[text()='額溫(Forehead)']")
+    temp_meas_mthd.click()
+    #temp_meas_mthd = driver.find_element_by_id("430334644_2860832441")
+    #temp_meas_mthd.send_keys(Keys.SPACE)
+
+    temp = driver.find_element_by_id("bt")
     temp.send_keys(body_temp)
-    symptom = driver.find_element_by_id("430334646_2860832447")
-    symptom.send_keys(Keys.SPACE)
-    confirm = driver.find_element_by_id("430334641_2860832427")
-    confirm.send_keys(Keys.SPACE)
-    take_med = driver.find_element_by_id("447437405_2965044714")
-    take_med.send_keys(Keys.SPACE)
+
+
+    take_med = driver.find_element_by_xpath("//select[@name='tookAntipyretics']/option[text()='否/No']")
+    take_med.click()
+    #take_med = driver.find_element_by_id("447437405_2965044714")
+    #take_med.send_keys(Keys.SPACE)
+
+    take_med = driver.find_element_by_xpath("//select[@name='symptoms']/option[text()='以上皆無/I do not have the above symptoms']")
+    take_med.click()
+    #symptom = driver.find_element_by_id("430334646_2860832447")
+    #symptom.send_keys(Keys.SPACE)
+
+
+
 
     assert "No results found." not in driver.page_source  # Debugger
 
 
 def confirm_click():
-    c = '.btn.small.next-button.survey-page-button.user-generated.notranslate'
-    send_btn = driver.find_element_by_css_selector(c)
-    send_btn.click()
+    confirm = driver.find_element_by_id("submit")
+    confirm.click()
+
+
+#    c = '.btn.small.next-button.survey-page-button.user-generated.notranslate'
+#    send_btn = driver.find_element_by_css_selector(c)
+#    send_btn.click()
 
 
 if __name__ == '__main__':
@@ -58,6 +73,6 @@ if __name__ == '__main__':
 
 #   driver.get_screenshot_as_file(r'./' + Timestamp + '.png')
 
-#   input("press anything to continue")
+#    input("press anything to continue")
     # close driver
     driver.close()
